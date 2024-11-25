699f92fbc3ea13f12c5f33696312aec2a5d6ddb8
nhat <nhat27072003@gmail.com>
2024-11-25 23:36:57 +0700

    student

--- app/src/main/java/vn/edu/hust/studentman/StudentAdapter.kt
+++ app/src/main/java/vn/edu/hust/studentman/StudentAdapter.kt
@@ -1,47 +1,29 @@
 package vn.edu.hust.studentman
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class StudentAdapter(
-  private val students: MutableList<StudentModel>,
-  private val onEditClick: (StudentModel) -> Unit, // Callback khi nhấn Edit
-  private val onRemoveClick: (StudentModel) -> Unit // Callback khi nhấn Remove
-) : RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+class StudentAdapter(context: Context, private val students: List<StudentModel>) :
+  ArrayAdapter<StudentModel>(context, 0, students) {
 
-  class StudentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val textStudentName: TextView = itemView.findViewById(R.id.text_student_name)
-    val textStudentId: TextView = itemView.findViewById(R.id.text_student_id)
-    val imageEdit: ImageView = itemView.findViewById(R.id.image_edit)
-    val imageRemove: ImageView = itemView.findViewById(R.id.image_remove)
-  }
-
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
-    val itemView = LayoutInflater.from(parent.context)
-      .inflate(R.layout.layout_student_item, parent, false)
-    return StudentViewHolder(itemView)
-  }
-
-  override fun getItemCount(): Int = students.size
-
-  override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+  override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_student_item, parent, false)
     val student = students[position]
 
-    holder.textStudentName.text = student.studentName
-    holder.textStudentId.text = student.studentId
+    // Thiết lập dữ liệu
+    val nameTextView = view.findViewById<TextView>(R.id.text_student_name)
+    val idTextView = view.findViewById<TextView>(R.id.text_student_id)
 
-    // Gọi callback khi nhấn nút chỉnh sửa
-    holder.imageEdit.setOnClickListener {
-      onEditClick(student)
-    }
+    nameTextView.text = student.studentName
+    idTextView.text = student.studentId
 
-    // Gọi callback khi nhấn nút xóa
-    holder.imageRemove.setOnClickListener {
-      onRemoveClick(student)
-    }
+    return view
   }
 }
+
